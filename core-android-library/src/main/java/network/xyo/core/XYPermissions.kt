e81acee453af2078c72f13be40673f3e9bb3e17a
Arie Trouw <arie.trouw@xyfindables.com>
2018-12-21 20:12:30 -0800

    Update XYPermissions.kt

--- core-android-library/src/main/java/network/xyo/core/XYPermissions.kt
+++ core-android-library/src/main/java/network/xyo/core/XYPermissions.kt
@@ -7,35 +7,46 @@ Created by arietrouw on 1/31/17.
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-class XYPermissions(private val activity: Activity) : XYBase() {
+class XYPermissions(private val context: Context) : XYBase() {
 
     fun checkPermissionForGallery(): Boolean {
-        val result = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE)
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE)
         return (result == PackageManager.PERMISSION_GRANTED)
     }
 
     fun checkPermissionForCamera(): Boolean {
-        val result = ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
         return (result == PackageManager.PERMISSION_GRANTED)
     }
 
     fun checkPermissionForBluetooth(): Boolean {
-        val result = ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH)
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH)
         return (result == PackageManager.PERMISSION_GRANTED)
     }
 
     fun checkPermissionForBluetoothAdmin(): Boolean {
-        val result = ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_ADMIN)
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_ADMIN)
         return (result == PackageManager.PERMISSION_GRANTED)
     }
 
     fun checkPermissionForBluetoothPrivilaged(): Boolean {
-        val result = ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_PRIVILEGED)
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_PRIVILEGED)
+        return (result == PackageManager.PERMISSION_GRANTED)
+    }
+
+    fun checkPermissionForCoarseLocation(): Boolean {
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+        return (result == PackageManager.PERMISSION_GRANTED)
+    }
+
+    fun checkPermissionForFineLocation(): Boolean {
+        val result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
         return (result == PackageManager.PERMISSION_GRANTED)
     }
 
@@ -44,25 +55,29 @@ class XYPermissions(private val activity: Activity) : XYBase() {
             return
         }
 
-        if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+        if (checkPermissionForCoarseLocation() && checkPermissionForFineLocation()) {
+            return
+        }
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
 
-                val alertDialog = AlertDialog.Builder(activity).create()
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context as Activity, permission)) {
+
+                val alertDialog = AlertDialog.Builder(context).create()
                 alertDialog.setTitle("Permission Needed")
                 alertDialog.setMessage(explainText)
                 alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK"
                 ) { dialog, _ ->
                     dialog.dismiss()
-                    ActivityCompat.requestPermissions(activity,
+                    ActivityCompat.requestPermissions(context,
                             arrayOf(permission),
                             reqCode)
                 }
-                if (!activity.isFinishing) {
+                if (!context.isFinishing) {
                     alertDialog.show()
                 }
             } else {
-                ActivityCompat.requestPermissions(activity,
+                ActivityCompat.requestPermissions(context,
                         arrayOf(permission),
                         reqCode)
             }
@@ -75,33 +90,37 @@ class XYPermissions(private val activity: Activity) : XYBase() {
             return
         }
 
-        if (ContextCompat.checkSelfPermission(activity, permissions[0])
+        if (checkPermissionForCoarseLocation() && checkPermissionForFineLocation()) {
+            granted()
+            return
+        }
+
+        if (ContextCompat.checkSelfPermission(context, permissions[0])
                 != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(activity, permissions[1])
+                && ContextCompat.checkSelfPermission(context, permissions[1])
                 != PackageManager.PERMISSION_GRANTED) {
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[0])
-                    && ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[1])) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context as Activity, permissions[0])
+                    && ActivityCompat.shouldShowRequestPermissionRationale(context, permissions[1])) {
 
-                val alertDialog = AlertDialog.Builder(activity).create()
+                val alertDialog = AlertDialog.Builder(context).create()
                 alertDialog.setTitle("Permission Needed")
                 alertDialog.setMessage(explainText)
                 alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK"
                 ) { dialog, _ ->
                     dialog.dismiss()
-                    ActivityCompat.requestPermissions(activity,
+                    ActivityCompat.requestPermissions(context,
                             permissions,
                             reqCode)
                     granted()
                 }
-                if (!activity.isFinishing) {
+                if (!context.isFinishing) {
                     alertDialog.show()
                 }
             } else {
-                ActivityCompat.requestPermissions(activity,
+                ActivityCompat.requestPermissions(context,
                         permissions,
                         reqCode)
-                granted()
             }
         }
     }
